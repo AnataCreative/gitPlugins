81573d733654a011b89fcf3d72bf67c61c977573
Ibe Vanmeenen <ibe@gents.be>
2016-05-06 17:21:31 +0200

    update

--- GitPluginsPlugin.php
+++ GitPluginsPlugin.php
@@ -3,30 +3,72 @@ namespace Craft;
 
 class GitPluginsPlugin extends BasePlugin
 {
-	function getName()
-	{
-		 return Craft::t('gitPlugins');
-	}
+    protected
+        $_version = '1.0.0',
+        $_schemaVersion = null,
+        $_name = 'gitPlugins',
+        $_url = 'https://github.com/AnataCreative/gitPlugins',
+        $_releaseFeedUrl = '',
+        $_documentationUrl = '',
+        $_description = '',
+        $_developer = 'Anata Creative',
+        $_developerUrl = 'https://anatacreative.com/',
+        $_minVersion = '2.0';
 
-	function getVersion()
-	{
-		return '1.0';
-	}
+    public function getName()
+    {
+        return Craft::t($this->_name);
+    }
 
-	function getDeveloper()
-	{
-		return 'Anata Creative';
-	}
+    public function getUrl()
+    {
+        return $this->_url;
+    }
 
-	function getDeveloperUrl()
-	{
-		return 'https://anatacreative.com/';
-	}
+    public function getVersion()
+    {
+        return $this->_version;
+    }
 
-	public function onBeforeInstall()
-	{
-		if (!file_exists('../craft/storage/uploads/gitPlugins')) {
-			mkdir('../craft/storage/uploads/gitPlugins', 0777, true);
-		}
-	}
+    public function getDeveloper()
+    {
+        return $this->_developer;
+    }
+
+    public function getDeveloperUrl()
+    {
+        return $this->_developerUrl;
+    }
+
+    public function getDescription()
+    {
+        return $this->_description;
+    }
+
+    public function getDocumentationUrl()
+    {
+        return $this->_documentationUrl;
+    }
+
+    public function getSchemaVersion()
+    {
+        return $this->_schemaVersion;
+    }
+
+    public function getReleaseFeedUrl()
+    {
+        return $this->_releaseFeedUrl;
+    }
+
+    public function getCraftRequiredVersion()
+    {
+        return $this->_minVersion;
+    }
+
+    public function onBeforeInstall()
+    {
+        if (!file_exists('../craft/storage/uploads/gitPlugins')) {
+            mkdir('../craft/storage/uploads/gitPlugins', 0777, true);
+        }
+    }
 }
--- README.md
+++ README.md
@@ -6,7 +6,6 @@ Image Resizer
 Image Resizer is a Craft plugin that resizes your assets when they are uploaded. This allows huge images to be resized so as not to use up unnecessary disk space, but still kept at a reasonable resolution. This plugin is not a replacement for using image transforms throughout your site.
 
 # ToDo's
-- check permissions `craft.users.can('accessPlugin-gitplugin');`
 - Add plugin template (not only widget)
 - Do Ajax submit
 - Optional redirect
